049a6e09a7ee1b5405f6171910cf4d18561a37d8
markusatskassov87 <markusatskassov87@gmail.com>
2023-12-21 12:44:59 +0200

    Ülesanne 3.

--- BogdanViblyy1.py
+++ BogdanViblyy1.py
@@ -1,1 +1,24 @@
-print("Tere maailm!")
+#print("Tere maailm!")
+#nimi=input("Mis on sinu nimi?") 
+#vanus=int(input("kui vana sa oled?")) #float()->2.5
+#print("Tere tulemast! "+nimi+". Sa oled"+str(vanus)+"aastat vana")
+#print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasta vana")
+#print("Tere tulemast! {0} Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+#2
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+
+#print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+#print("Muutuja eesnimi=",eesnimi,"tüüp on",type(eesnimi))
+#print("Muutuja pikkus=",pikkus,"tüüp on",type(pikkus))
+#print("Muutuja kas_käib_kooli=",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
+#3
+from random import *
+kokku=randint(10,100)
+print("Kokku: ",kokku)
+mitu=int(input("Mitu kommi tahad võtta?"))
+kokku=mitu
+print("Nüüd laua peal on", kokku,"kommid")
